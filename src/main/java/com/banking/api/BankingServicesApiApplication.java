6eeef4edeeed8640b865c498c00aff1116b943fa
Lilian G. <97319339+gimenezt@users.noreply.github.com>
2025-04-07 23:35:34 -0300

    feat: setup inicial do projeto com Maven e estrutura base
    - adicionado arquivo pom.xml com dependências do spring boot e h2
    - criada estrutura de diretórios
    - adicionado arquivo principal

---
+++ src/main/java/com/banking/api/BankingServicesApiApplication.java
@@ -0,0 +1,13 @@
+package com.banking.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingServicesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingServicesApiApplication.class, args);
+    }
+}
+
