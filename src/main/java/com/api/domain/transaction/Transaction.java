e8e6dd2bcef5e883a18c04dc38abc4841e6ec666
Lilian G. <97319339+gimenezt@users.noreply.github.com>
2025-04-09 19:08:54 -0300

    feat: cria classe de transações
    - criação da classe de transações Transaction com atributos id, accountOrigin, accountDestination, amount, transactionStatus, datetime

---
+++ src/main/java/com/api/domain/transaction/Transaction.java
@@ -0,0 +1,46 @@
+package com.api.domain.transaction;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String accountOrigin;
+
+    @NotBlank
+    private String accountDestination;
+
+    @NotBlank
+    private String transactionStatus;
+
+    @NotNull
+    private Double amount;
+
+    @NotNull
+    private LocalDateTime datetime = LocalDateTime.now();
+
+    // getters e setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getAccountOrigin() { return accountOrigin; }
+    public void setAccountOrigin(String accountOrigin) { this.accountOrigin = accountOrigin; }
+
+    public String getTransactionStatus() { return transactionStatus; }
+    public void setTransactionStatus(String transactionStatus) { this.transactionStatus = transactionStatus; }
+
+    public String getAccountDestination() { return accountDestination; }
+    public void setAccountDestination(String accountDestination) { this.accountDestination = accountDestination; }
+
+    public Double getAmount() { return amount; }
+    public void setAmount(Double amount) { this.amount = amount; }
+
+    public LocalDateTime getDatetime() { return datetime; }
+}
