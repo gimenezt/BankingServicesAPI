0a135433ad31f31dbc2c6553e9f7ee31af28080b
Lilian G. <97319339+gimenezt@users.noreply.github.com>
2025-04-14 00:49:21 -0300

    refactor: método de TransactionBuilder para criar transações em DTO

--- src/main/java/com/api/business/TransactionBuilder.java
+++ src/main/java/com/api/business/TransactionBuilder.java
@@ -4,15 +4,21 @@ import com.api.model.dto.TransactionDTO;
 import com.api.model.entity.Transaction;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
 @Component
 public class TransactionBuilder {
 
     // Cria a instância de transaction
     public Transaction build(TransactionDTO dto) {
+        return build(dto.getAccountOrigin(), dto.getAccountDestination(), dto.getAmount());
+    }
+
+    public Transaction build(String accountOrigin, String accountDestination, Double amount) {
         Transaction transaction = new Transaction();
-        transaction.setAccountOrigin(dto.getAccountOrigin());
-        transaction.setAccountDestination(dto.getAccountDestination());
-        transaction.setAmount(dto.getAmount());
+        transaction.setAccountOrigin(accountOrigin);
+        transaction.setAccountDestination(accountDestination);
+        transaction.setAmount(amount);
         return transaction;
     }
 }
